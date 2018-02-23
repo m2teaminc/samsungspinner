a37951daf4ef67bfb24915ed2078eff10c3aacec
M2 Team <m2team.inc@gmail.com>
2018-02-23 17:23:37 +0700

    Add missing file dark theme options

--- myapplication/src/main/java/com/hm/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/hm/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.hm.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Spinner;
 
 import com.hm.samsungspinner.CustomSpinner;
@@ -10,19 +11,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    CustomSpinner spinner;
+    private boolean isDark;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CustomSpinner spinner = findViewById(R.id.spinner);
+        spinner = findViewById(R.id.spinner);
         List<String> datas = new ArrayList<>();
         datas.add("Hanoi");
         datas.add("Haiphong");
         datas.add("Danang");
         datas.add("HCM");
         spinner.setData(datas);
-        spinner.setDarkTheme(true);
+    }
+
+    public void changeTheme(View view) {
+        isDark = !isDark;
+        spinner.setDarkTheme(isDark);
     }
 }
